7e45c9fc8ea2e09bb68d852d63a1ac5091f5c25a
Otavio Mascarenhas <otaviomascarenhaspessoal@gmail.com>
2024-04-16 01:01:29 -0300

    feat(env): put localhost

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,4 +1,4 @@
-export const DEV_API = 'http://192.168.0.104'
+export const DEV_API = 'http://localhost'
 export const PORT_API = 8000
 
 // Here you can just switch the variables to the correct environment.
